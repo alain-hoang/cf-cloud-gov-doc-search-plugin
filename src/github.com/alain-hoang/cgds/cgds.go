169ee5ca8c33a6d2dee8a28691113653fdfe14be
Alain Hoang <alain.hoang@gsa.gov>
2017-02-16 16:53:10 -0600

    Addresses #4
    * Allow cli uri override

--- src/github.com/alain-hoang/cgds/cgds.go
+++ src/github.com/alain-hoang/cgds/cgds.go
@@ -8,19 +8,22 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"runtime"
 	"os"
+	"flag"
+	"encoding/json"
 )
 
 type CloudGovDocSearchPlugin struct{}
 
 
 type SearchResult struct {
-	Url      string
-	Descr    string
+	Url      string      `json:"url"`
+	Descr    string      `json:"description"`
 }
 
-const queryUrl = "https://search.usa.gov/search?affiliate=cloud.gov&utf8=%E2%9C%93&query="
+const QUERY_URL = "https://search.usa.gov/search?affiliate=cloud.gov&utf8=%E2%9C%93&query="
+const OPTIONS_HELP_STR = "[-format <human|json>] [-url <uri>] <search-term>"
 
-func getResults(searchTerm string) []SearchResult {
+func getResults(searchTerm string, queryUrl string) []SearchResult {
 	doc, err := goquery.NewDocument(queryUrl + searchTerm)
 	if err != nil {
 		fmt.Printf("Error reading %v\n", err)
@@ -45,9 +48,21 @@ func parseResults(doc *goquery.Document) []SearchResult {
 	return res
 }
 
-func printResults(sr []SearchResult) {
-	for idx, r := range sr {
-		fmt.Printf("%d | %s | %s\n", idx, r.Url, r.Descr)
+func printResults(sr []SearchResult, format string) {
+	switch format {
+	case "human":
+		for idx, r := range sr {
+			fmt.Printf("%d | %s | %s\n", idx, r.Url, r.Descr)
+		}
+	case "json":
+		js, err := json.Marshal(sr)
+		if err != nil {
+			fmt.Printf("Error to json: %v", err)
+			return
+		}
+		fmt.Printf("%s\n", js)
+	default:
+		fmt.Printf("Don't know how to output that")
 	}
 }
 
@@ -93,16 +108,50 @@ func openUrl(url string) error {
 	return err
 }
 
+func canFormat(format string) bool {
+	switch format {
+	case "human":
+		return true
+	case "json":
+		return true
+	default:
+		return false
+	}
+}
+
+
+
+func usage() {
+	fmt.Printf("cf cloud-gov-doc-search %s\n", OPTIONS_HELP_STR) 
+}
+
 func (c *CloudGovDocSearchPlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	// Ensure that we called the command basic-plugin-command
 	if args[0] == "cloud-gov-doc-search" {
-		if len(args) > 1 {
-			sr := getResults(args[1])
-			printResults(sr)
-			err := chooseFromResults(sr)
-			if err != nil {
-				os.Exit(1)
+		flags := flag.NewFlagSet("cgds-args", flag.ExitOnError)
+		urlPtr := flags.String("url", QUERY_URL, "search url")
+		fmtPtr := flags.String("format", "human", "Output format <human|json>")
+
+		err := flags.Parse(args[1:])
+		if err != nil {
+			usage()
+			fmt.Printf("Error parsing:  %v\n", err)
+			os.Exit(1)
+		}
+		if canFormat(*fmtPtr) {
+			sq := flags.Arg(0)
+			sr := getResults(sq, *urlPtr)
+			printResults(sr, *fmtPtr)
+			if *fmtPtr == "human" {
+				err = chooseFromResults(sr)
+				if err != nil {
+					fmt.Printf("Error in choosing: %v\n", err)
+					os.Exit(1)
+				}
 			}
+		} else {
+			usage()
+			os.Exit(1)
 		}
 	}
 }
@@ -126,13 +175,15 @@ func (c *CloudGovDocSearchPlugin) GetMetadata() plugin.PluginMetadata {
 				Alias:    "cgds",
 				HelpText: "Search cloud.gov documentation for search-term",
 				UsageDetails: plugin.Usage{
-					Usage: "cgds-plugin\n   cf cgds-plugin search-term",
+					Usage: "cgds-plugin\n   cf cgds-plugin " + OPTIONS_HELP_STR,
+					Options: map[string]string{
+						"url": "If this param is set ",
+					},
 				},
 			},
 		},
 	}
 }
-
 
 func main() {
 	plugin.Start(new(CloudGovDocSearchPlugin))
